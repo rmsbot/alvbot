3ceb1c4e4a7770d98187232465c3d419de840d0d
rmsbot <74935140+rmsbot@users.noreply.github.com>
2020-12-16 00:06:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -733,14 +733,7 @@ axios.get(`http://inspirobot.me/api?generate=true`).then((res) => {
     conn.sendMessage(id, hasil ,MessageType.text);
 })
 }
-else if (text == '%ptl'){
-conn.sendMessage(id, 'kirim #ptl cewek/cowok\n\nContoh: #ptl cewek' ,MessageType.text);
-}
-   if (messageType == 'imageMessage')
-   {
-      let caption = imageMessage.caption.toLocaleLowerCase()
-      const buffer = await conn.downloadMediaMessage(m) // to decrypt & use as a buffer
-      if (caption == '%sticker')
+  if (caption == '%sticker')
       {
          const stiker = await conn.downloadAndSaveMediaMessage(m) // to decrypt & save to file
 
